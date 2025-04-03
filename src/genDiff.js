0dea28b20053f4672e755d97ddbd91829838b94c
Андрей Зуйков <andrei.zuikov@yandex.ru>
2025-04-03 11:32:49 +0300

    fix 1 check

--- src/genDiff.js
+++ src/genDiff.js
@@ -2,7 +2,7 @@ import { extname } from 'path';
 import readFile from './utils.js';
 import parse from './parsers.js';
 import comparer from './comparer.js';
-import formatter from './formatters/index.js';
+import outputController from './formatters/index.js';
 
 const genDiff = (file1, file2, format) => {
   const fileContent1 = readFile(file1);
@@ -16,7 +16,7 @@ const genDiff = (file1, file2, format) => {
 
   const comparedFiles = comparer(parsedData1, parsedData2);
 
-  const resultDiff = formatter(comparedFiles, format);
+  const resultDiff = outputController(comparedFiles, format);
   return resultDiff;
 };
 
